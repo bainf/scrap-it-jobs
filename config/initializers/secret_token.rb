ef85151dcf8b2e84d96d4ddeeb006a3ea36a97c1
SAIDI Belkacem <bainf@hotmail.fr>
2013-07-04 08:10:37 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OdeskRails::Application.config.secret_token = '4c9587662cf7882709b1833fa910802d089456ee70b926316db1e4b19fe3607753cca3f8079751a77a4e479b1d5bdf2006319e711944c6e49d5bd9474a54eab8'
